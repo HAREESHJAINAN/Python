e423bc79e5baebb03a38f0c5ddde3ce6eed1a3bc
Hareesh Jainan <your-email@example.com>
2025-03-01 13:49:57 +0530

    Operators

---
+++ Learning/Operators.py
@@ -0,0 +1,89 @@
+"""
+Python Operators
+Operators are used to perform operations on variables and values.
+
+In the example below, we use the + operator to add together two values:
+
+Example
+print(10 + 5)
+"""
+#print(10 + 5)
+
+"""
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+"""
+
+"""
+
+Python Arithmetic Operators
+Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+
++	Addition	                  x + y	
+-	Subtraction	                  x - y	
+*	Multiplication	              x * y	
+/	Division	                  x / y	
+%	Modulus	                      x % y	
+**	Exponentiation	              x ** y	
+//	Floor division	              x // y
+
+"""
+x = 50
+y = 10
+
+# //	Floor division	              x // y        #Floor division means Bhagahaaram, That to nearest integer. ex: 7//2 = 3.5 ==> 3
+print("Floor Division", x//y)
+print("#Floor division means Bhagaphalam, That too nearest integer. ex: 7//2 = 3.5 ==> 3")
+print("------------------------------------------------------------------------------------------")
+
+#  **	Exponentiation	              x ** y	#x = 2, y=5, print(x ** y) #same as 2*2*2*2*2
+e1=2
+e2=5
+
+print("Exponentiation: ", e1**e2)  #same as 2*2*2*2*2
+print("Exponentiation means e2 is power to the e1")
+print("------------------------------------------------------------------------------------------")
+
+#  %	Modulus	                      x % y	  #Modulus means shesham, Reminder.  5%2=1
+m1=5
+m2=2
+print("Modulus: ",m1%m2)
+print("Modulus means reminder")
+print("------------------------------------------------------------------------------------------")
+
+#  /	Division	                  x / y	
+x = 50
+y = 10
+
+print("Division: ", x/y)
+print("------------------------------------------------------------------------------------------")
+
+
+#  *	Multiplication	              x * y	
+x = 50
+y = 10
+
+print("Multiplication: ", x*y)
+print("------------------------------------------------------------------------------------------")
+
+#  -	Subtraction	                  x - y	
+x = 50
+y = 10
+print("Subtraction: ", x-y)
+print("------------------------------------------------------------------------------------------")
+
+
+#  +	Addition	                  x + y	
+x = 50
+y = 10
+print("Addition: ", x+y)
+print("------------------------------------------------------------------------------------------")
